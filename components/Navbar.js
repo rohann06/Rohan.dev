547a6d21aae4d25ed21e7724d350a9dc18896fe0
Rohan <rohanahire651@gmail.com>
2023-03-20 00:40:02 +0530

    ccss change in responsive nav section

--- components/Navbar.js
+++ components/Navbar.js
@@ -7,30 +7,30 @@ const Navbar = () => {
   const[navOpen, setNavOpen] = useState(false);
 
   return (
-    <nav className=" lg:flex justify-between items-center shadow-md w-full py-[20px] px-[40px]">
+    <nav className=" lg:flex justify-between items-center shadow-md w-full py-[18px] px-[40px]">
 
       <div className=" flex gap-48 items-center">
         <Link href={'/'}>
-          <h1 className=" text-[1.5rem] lg:text-[1.7rem] cursor-pointer font-semibold">Rohan.dev</h1>
+          <h1 className=" text-[1.3rem] lg:text-[1.5rem] cursor-pointer font-bold">Rohan.dev</h1>
         </Link>
         <p onClick={() => setNavOpen(!navOpen)}  className=" lg:hidden text-2xl">
             <GiHamburgerMenu/>
         </p>
       </div>
 
-      <div className={` ${navOpen?"-translate-x-full" : "-translate-x-0"} lg:translate-x-0 bg-white absolute left-0 top-0 text-center w-full lg:w-auto lg:static lg:flex items-center gap-8 font-semibold transition-all duration-500 ease-in-out lg:p-0`}>
+      <div className={` ${navOpen?"-translate-x-full" : "-translate-x-0"} lg:translate-x-0 bg-white absolute left-0 top-0 bottom-0 text-center w-full lg:w-auto lg:static lg:flex items-center gap-8 font-semibold text-lg  transition-all duration-500 ease-in-out lg:p-0 `}>
 
         <p onClick={() => setNavOpen(!navOpen)}  className=" lg:hidden  text-3xl flex justify-end py-[20px] px-[40px]">
             <RxCross2/>
         </p>
-        <p onClick={() => setNavOpen(!navOpen)} className=" cursor-pointer hover:text-[#147efb] duration-300 text-lg my-10 lg:my-0">Home</p>
+        <p onClick={() => setNavOpen(!navOpen)} className=" cursor-pointer hover:text-[#147efb] duration-300 my-10 lg:my-0">Home</p>
 
-        <p onClick={() => setNavOpen(!navOpen)} className=" cursor-pointer hover:text-[#147efb] duration-300 text-lg my-10 lg:my-0">About</p>
+        <p onClick={() => setNavOpen(!navOpen)} className=" cursor-pointer hover:text-[#147efb] duration-300 my-10 lg:my-0">About</p>
 
-        <p onClick={() => setNavOpen(!navOpen)} className=" cursor-pointer hover:text-[#147efb] duration-300 text-lg my-10 lg:my-0">Projec</p>
+        <p onClick={() => setNavOpen(!navOpen)} className=" cursor-pointer hover:text-[#147efb] duration-300 my-10 lg:my-0">Projec</p>
 
-        <p onClick={() => setNavOpen(!navOpen)} className=" cursor-pointer hover:text-[#147efb] duration-300 text-lg my-10 lg:my-0">Contact</p>
-        
+        <p onClick={() => setNavOpen(!navOpen)} className=" cursor-pointer hover:text-[#147efb] duration-300 my-10 lg:my-0">Contact</p>
+
       </div>
 
     </nav>
