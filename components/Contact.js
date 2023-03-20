5d5bdad2c7d6bd9d6ffc9aae37e1a566aa176226
Rohan <rohanahire651@gmail.com>
2023-03-20 14:59:27 +0530

    Contact section done

---
+++ components/Contact.js
@@ -0,0 +1,53 @@
+/* eslint-disable react/no-unescaped-entities */
+import React from "react";
+import { FaMapMarkedAlt } from "react-icons/fa";
+import { IoIosMailOpen } from "react-icons/io";
+
+const Contact = () => {
+  return (
+    <div className=" py-[8rem]">
+      <div>
+        <p className=" uppercase text-[#147efb] font-bold mb-[1rem] text-[17px]">
+          Contact
+        </p>
+        <h1 className=" text-[25px] font-bold">Don't be shy! Hit me up! 👇</h1>
+      </div>
+
+      <div className=" flex flex-col lg:flex-row items-center gap-x-[8rem] gap-y-[5rem] mt-[4rem]">
+
+        <div className=" flex flex-col lg:flex-row items-center text-center gap-4">
+          <div className=" rounded-full shadow-lg p-5">
+            <p className=" text-[#147efb] text-[30px]">
+              <FaMapMarkedAlt />
+            </p>
+          </div>
+          <div className=" lg:text-left text-[17px] ">
+            <p className=" font-bold">Locatin</p>
+            <p className=" text-[#8e8e8e] hover:text-[#147efb] duration-300 cursor-pointer">
+              Surat, India
+            </p>
+          </div>
+        </div>
+
+        <div className=" flex flex-col lg:flex-row items-center gap-4">
+          <div className=" rounded-full shadow-lg p-5">
+            <p className=" text-[#147efb] text-[30px]">
+              <IoIosMailOpen />
+            </p>
+          </div>
+          <div className=" lg:before:text-left text-[17px] ">
+            <p className=" font-bold">Locatin</p>
+            <a
+              href="mailto:rohanahire006@gmail.com"
+              className=" text-[#8e8e8e] hover:text-[#147efb] duration-300"
+            >
+              rohanahire006@gmail.com
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Contact from "../components/Contact";
 
 export default function Home() {
   return (
@@ -9,9 +10,8 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <div>
-        </div>
+      <main className=" mx-3 lg:mx-80 text-center lg:text-left">
+        <Contact />
       </main>
     </div>
   );
