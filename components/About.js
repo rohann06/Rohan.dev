4dc7b61b4fb7ef13b270c3e98e535fa0644a6622
Rohan <rohanahire651@gmail.com>
2023-03-20 16:32:47 +0530

    About section done

---
+++ components/About.js
@@ -0,0 +1,38 @@
+import Image from "next/image";
+import React from "react";
+import img from "../public/about.webp";
+
+const About = () => {
+  return (
+    <div className=" flex flex-col lg:flex-row justify-center items-center gap-5 py-[8rem] lg:py-[15rem]">
+      <div>
+        <Image
+          src={img}
+          height={200}
+          width={500}
+          alt="about img"
+          className=" rounded-2xl"
+        />
+      </div>
+      <div className=" text-center lg:text-left lg:px-10 lg:w-[600px]">
+        <p className=" uppercase text-[#147efb] font-bold mb-[1rem] text-[17px]">
+          Contact
+        </p>
+        <h1 className=" text-[25px] font-bold mb-[1rem]">
+          A dedicated Front-end Developer based in surat, India 📍
+        </h1>
+        <p className=" text-[#8e8e8e]">
+          As a Junior Front-End Developer, I possess an impressive arsenal of
+          skills in <span className=" text-black font-medium">Next.js , JavaScript , Typescript, Tailwind CSS and PostgreSQL</span> . I excel in build and maintaining responsive websites that
+          offer a smooth user experience. My expertise lies in crafting dynamic,
+          engaging interfaces through writing clean and optimized code and
+          utilizing cutting-edge development tools and techniques. I am also a
+          team player who thrives in collaborating with cross-functional teams
+          to produce outstanding web applications.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import About from "../components/About";
 import Contact from "../components/Contact";
 
 export default function Home() {
@@ -11,6 +12,7 @@ export default function Home() {
       </Head>
 
       <main className=" mx-3 lg:mx-80 text-center lg:text-left">
+        <About/>
         <Contact />
       </main>
     </div>
