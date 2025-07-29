3051607a3711dc12a7348461f6411886de9a3528
Rohan Ahire <rohanahire006@gmail.com>
2025-07-29 16:32:55 +0530

    Added a new projects in projectdb

--- utils/_projectdb.js
+++ utils/_projectdb.js
@@ -8,4 +8,41 @@ export const projects = [
     description:
       "This is an e-commerce clothing website I built for my own brand. I created it to learn full-stack development while starting a small side business.",
   },
+  {
+    id: "1",
+    title: "Crowd Funding",
+    githubUrl: "https://github.com/rohann06/Crowd-Funding",
+    projectImageUrl: "/projects/crowdFund.png",
+    appUrl:
+      "https://crowd-funding-cm1n-71uuqbmgn-rohana6s-projects.vercel.app/",
+    description:
+      "Built a decentralized crowdfunding platform where users can create and contribute to campaigns directly on the blockchain.Tech syack I've used was Next.js, Wagmi, RainbowKit, Solidity",
+  },
+  {
+    id: "2",
+    title: "Posts",
+    githubUrl: "https://github.com/rohann06/Posts",
+    projectImageUrl: "/projects/posts.png",
+    appUrl: "https://posts-blush-eight.vercel.app/",
+    description:
+      "Built a Web3-powered dApp enabling users to connect Ethereum wallets (via RainbowKit) to create and explore posts on-chain.Developed with Solidity smart contracts, Next.js, Wagmi, and RainbowKit for a seamless decentralized experience.",
+  },
+  {
+    id: "3",
+    title: "D_D Academy",
+    githubUrl: "https://github.com/Developer-DAO/academy-turbo",
+    projectImageUrl: "/projects/academy.png",
+    appUrl: "https://academy.developerdao.com/",
+    description:
+      "Contributed to Developer DAO Academy, an open-source platform offering free web3 and blockchain educational resources.",
+  },
+  {
+    id: "4",
+    title: "Make your meal",
+    githubUrl: "https://github.com/rohann06/make_your_meal",
+    projectImageUrl: "/projects/mym.png",
+    appUrl: "https://make-your-meal-git-main-rohana6s-projects.vercel.app/",
+    description:
+      "This is a Next.js project I made to learn about the APIs and how to fetch, filter, and managis the data we receive from teh APi",
+  },
 ];
